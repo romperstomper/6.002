52a5b6190f8f0883095fc07116802dab5f1faaa3
garyboland@gmail.com <garyboland@gmail.com>
2015-08-02 15:43:16 +0100

    simulation with drugs

--- w4-b.py
+++ w4-b.py
@@ -8,13 +8,19 @@ class ResistantVirus(SimpleVirus):
     self.getclearprob = getclearprob
     self.maxbirthprob = maxbirthprob
     
+  def isResistantTo(self, drug):
+    return self.resdict.get(drug)
+    
   def reproduce(self, popDensity, drugtreatment):
     inherit = {}
     for drug in drugtreatment:
-      if not resdict[drug]:
+      if not self.resdict[drug]:
         raise NoChildException
-      if random.random < mutprob:
-        inherit[drug]= True
+    for res in self.resdict:
+      if random.random() > self.mutprob:
+        inherit[res]= self.resdict[res]
+      else:
+        inherit[res]= not self.resdict[res]
 
     if random.random() < self.maxbirthprob*(1-popDensity):
       return ResistantVirus(self.maxbirthprob, self.getclearprob, inherit, self.mutprob)
